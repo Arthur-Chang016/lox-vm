269e1abe4d16a44043a0f20e9f0038ef119448b6
Arthur-Chang016 <b05505016@g.ntu.edu.tw>
2023-10-09 04:46:34 +0800

    Add disassembleChunk(), enum OpCode

---
+++ src/debug.rs
@@ -0,0 +1,9 @@
+use crate::chunk;
+
+fn disassembleChunk(chunk: []) {
+    printf("== %s ==\n", name);
+}
+
+fn disassembleInstruction() ->i32 {
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::mem;
+
+use crate::chunk::OpCode;
+
+pub mod chunk;
+
 fn main() {
+    let mut chunk: Vec<OpCode> = Vec::new();
+    
+    // let tmp = mem::size_of::<OpCode>();
+    
+    // println!("{tmp}");
+    
+    chunk.push(OpCode::OpReturn);
+    
+    // println!("{chunk. }");
+    
     println!("Hello, world!");
 }
